188da953785ac0f890a8fbcea38deb0ebd8cff31
WallisonRib <ribeirosantoswallison@gmail.com>
2024-06-12 00:07:14 -0300

    fix

--- config/database.js
+++ config/database.js
@@ -1,18 +1,22 @@
-const mysql = require('mysql2');
+const { Pool } = require('pg');
 
-const connection = mysql.createConnection({
-  host: process.env.HOST ,
+const pool = new Pool({
+  host: process.env.HOST,
   user: process.env.USER,
-  password: process.env.PASSWORD ,
-  database: process.env.DATABASE
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,  // Adicione a porta se necessário, por padrão é 5432
+  ssl: {
+    rejectUnauthorized: false,  // Use SSL se necessário
+  }
 });
 
-connection.connect(err => {
+pool.connect(err => {
   if (err) {
     console.error('Erro ao conectar ao banco de dados:', err);
     return;
   }
-  console.log('Conectado ao banco de dados MySQL!');
+  console.log('Conectado ao banco de dados PostgreSQL!');
 });
 
-module.exports = connection;
+module.exports = pool;
