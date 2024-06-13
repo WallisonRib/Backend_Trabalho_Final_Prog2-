0b36ccc5ba2e88b06053e3ed2426fbcdee4dccca
WallisonRib <ribeirosantoswallison@gmail.com>
2024-06-12 21:53:43 -0300

    fix: local ip to cors

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-
+// Configurar CORS para permitir requisições de 'http://localhost:5173'
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 
