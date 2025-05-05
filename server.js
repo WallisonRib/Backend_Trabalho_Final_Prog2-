c55f92524357c3d6d4e02d3df9aeb3863c217dca
WallisonRib <ribeirosantoswallison@gmail.com>
2025-05-05 19:47:41 -0300

    fix: add vercel.json

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
+// server.js
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 const routes = require('./routes');
 
 const app = express();
-
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-  console.log(`📚 Documentação Swagger: http://localhost:${PORT}/api-docs`);
-});
+module.exports = app;
